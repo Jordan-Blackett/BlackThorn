3463c3cf71786bb0553b634808083cddb6de5776
Jordan-Blackett <Jordan.blackett@gmail.com>
2019-07-01 13:12:09 +0100

    Premake

---
+++ premake5.lua
@@ -0,0 +1,108 @@
+workspace "BlackThorn"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- BLACKTHORN
+project "BlackThorn"
+	location "BlackThorn"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BT_PLATFORM_WINDOWS",
+			"BT_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/WhiteThorn")
+		}
+
+	filter "configurations:Debug"
+		defines "BT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BT_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BT_DIST"
+		optimize "On"
+
+-- WHITETHORN
+project "WhiteThorn"
+	location "WhiteThorn"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"BlackThorn/vendor/spdlog/include",
+		"BlackThorn/src"
+	}
+
+	links 
+	{
+		"BlackThorn"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BT_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "BT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BT_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BT_DIST"
+		optimize "On"
